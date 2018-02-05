bfc1e3bccccf7672483f0b86e3b001025065194f
Eric <theericlau@gmail.com>
2018-02-04 21:52:40 -0800

    factorial complete

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -3,3 +3,11 @@ Implement factorial.
 
 factorial(5) => 5*4*3*2*1 => 120
 */
+
+const factorial = (number) => {
+  if (number === 0) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+console.log(factorial(3));
