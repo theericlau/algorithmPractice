23e49ecc24d8f8453463b7caa933d6c94aacac8f
Eric <theericlau@gmail.com>
2018-02-04 22:14:46 -0800

    Pseudocoded flatten

--- recursion/flatten.js
+++ recursion/flatten.js
@@ -4,7 +4,13 @@ Implement a function that flattens a nested array.
 flatten([1,[2],[3, [[4]]]]);
 => [1,2,3,4]
 */
+const check = [1, [2], [3, [[4]]]];
 
 const flatten = array => {
-
+  //check each index to see if it's an array
+    //if it's an array
+      //reduce it and recurse
+  // return array
 };
+
+console.log(flatten(check));
