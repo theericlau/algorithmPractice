f4a5a7a86caa29266c71ecd88f59d31ee0a42223
Eric <theericlau@gmail.com>
2018-02-06 21:49:33 -0800

    fibonacci completed

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -8,3 +8,14 @@ output   0 1 1 2 3 5 8 13 21 34 ...
 
 What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
 */
+
+const fibonacci = n => {
+  if (n === 0) {
+    return 0;
+  } else if ( n <= 2) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(3));
