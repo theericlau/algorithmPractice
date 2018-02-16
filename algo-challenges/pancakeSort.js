f5003b895c1ae59644a62175508d06fada39a0dd
Eric <theericlau@gmail.com>
2018-02-15 22:08:00 -0800

    created helper functions flip and findlargest

--- algo-challenges/pancakeSort.js
+++ algo-challenges/pancakeSort.js
@@ -11,3 +11,57 @@ input:  arr = [1, 5, 4, 3, 2]
 output: [1, 2, 3, 4, 5]
 
 */
+
+function pancakeSort(arr) {
+  // your code goes here
+  //endpointer = array.length - 1;
+  let beginpointer = 0;
+  let endpointer = arr.length - 1;
+  //loop in to array
+  for (let i = 0; i < arr.length; i++) {
+    //find largestNumber in array
+    let k = findLargest(arr.slice(i)) + i;
+    //flip
+    flip(arr, k);
+    //first index of array swap with endpointer
+    let temp = arr[beginpointer];
+    arr[beginpointer] = arr[endpointer]
+    arr[endpointer] = temp;
+    flip(arr, arr.length - 1);
+    //decrement the endpointer
+    //check for next largest number
+  }
+  return arr;
+}
+
+const findLargest = arr => {
+  let maxNumIndex = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[maxNumIndex] < arr[i]) {
+      maxNumIndex = i;
+    }
+  }
+  return maxNumIndex;
+}
+
+const flip = (arr, k) => {
+  //endpoint = k
+  let endpoint = k;
+  //beginpoint = 0
+  let beginpoint = 0;
+  //stop when end = begin
+  while (endpoint >= beginpoint) {
+    let temp = arr[beginpoint];
+    arr[beginpoint] = arr[endpoint];
+    arr[endpoint] = temp;
+    endpoint--;
+    beginpoint++;
+  }
+  //flip will take k swap with first index
+  //decrement k
+  //increment beginpoint
+
+  //return array
+  return arr;
+}
