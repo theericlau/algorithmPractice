e550900ec7a21c4ce7b809dcccbb8389106f9c27
Eric <theericlau@gmail.com>
2018-02-17 15:18:46 -0800

    Attempted reverseWords

--- algo-challenges/reverseWords.js
+++ algo-challenges/reverseWords.js
@@ -9,11 +9,20 @@ the words in-place.
 var message = 'find you will pain only go you recordings security the into if';
 const reverseWords = message => {
   //split message into array by spaces
+  let array = message.split(' ');
   //init beginpointer
+  let beginPoint = 0;
   //init endpointer
-  //swap message at begin pointer with endpointer
-  //stop swap when endpointer is at begin pointer
+  let endPoint = array.length - 1;
+  while (endpoint > beginPoint) {
+    //stop swap when endpointer is at begin pointer
+    //swap message at begin pointer with endpointer
+    swap(array, beginPoint, endPoint);
+    beginPoint++;
+    endPoint--;
+  }
   //join message together
+  return array.join(' ');
 }
 console.log(reverseWords(message));
 
