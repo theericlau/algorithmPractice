46290ff6f7f6023ffc65f184bdddd9234cd40f75
Eric <theericlau@gmail.com>
2018-02-17 14:49:52 -0800

    init reverseWords

---
+++ algo-challenges/reverseWords.js
@@ -0,0 +1,11 @@
+/*
+Write a function reverseWords() that takes a string message and reverses the order of
+the words in-place.
+
+
+
+*/
+
+var message = 'find you will pain only go you recordings security the into if';
+reverseWords(message);
+// returns: 'if into the security recordings you go only pain will you find'
