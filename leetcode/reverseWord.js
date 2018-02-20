e79f6e7d58d384e3a11cc9252093af3d21cc0074
Eric <theericlau@gmail.com>
2018-02-19 23:12:33 -0800

    Completed

--- leetcode/reverseWord.js
+++ leetcode/reverseWord.js
@@ -15,7 +15,7 @@ Example 3:
 Input: 120
 Output: 21
  */
-var reverse = function (x) {
+const reverse = (x) => {
   if (Math.sign(x) === -1) {
     return reverseNumber(Math.abs(x)) * -1;
   } else {
@@ -23,4 +23,16 @@ var reverse = function (x) {
   }
 };
 
-console.log(reverse(-123));
+const reverseNumber = (x) => {
+  let num = x.toString().split('');
+  let begin = 0;
+  let end = num.length-1;
+  while (end > begin) {
+    [num[begin], num[end]] = [num[end],num[begin]];
+    begin++;
+    end--;
+  }
+  return Number(num.join(''));
+}
+
+console.log(reverse(-123456));
