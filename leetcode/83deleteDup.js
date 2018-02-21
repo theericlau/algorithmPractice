a0b813c3d0c38af112414e8c6b2a513300214508
Eric <theericlau@gmail.com>
2018-02-20 20:35:52 -0800

    errored solution

--- leetcode/83deleteDup.js
+++ leetcode/83deleteDup.js
@@ -11,5 +11,21 @@
  */
 
 var deleteDuplicates = function (head) {
-
+    // let node = head;
+    // if (node.next && node.next.val !== node.val) {
+    //     return node.next;
+    // }
+    // while (node) {
+    //     if (node.next && node.next.val === node.val) {
+    //         node.next = deleteDuplicates(node.next);
+    //     }
+    //     else if (node.next && node.next.val !== node.val) {
+    //         return node.next;
+    //     } else if (!node.next) {
+    //         return node.next;
+    //     }
+    //     node = node.next;
+    // }
+    // console.log('im head', head);
+    // return head;
 }
