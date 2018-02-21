4ec041b16da771abe6b7cf629dcacbf6cf23eea1
Eric <theericlau@gmail.com>
2018-02-20 19:15:31 -0800

    pseudocode solution

--- IC/productOf3.js
+++ IC/productOf3.js
@@ -14,5 +14,17 @@ We can do this in O(n) time and O(1) space.
 
 */
 
+const arr = [-10, -10, 1, 3, 2];
 
+const productOf3 = (arr) => {
+  //keep track of the smallest
+  //keep track of the highest
+  //keep track of the product of first 3
+  //keep track of the min product of 2
+  //keep track of the high product of 2
+  //loop into the array and
+    //check if current times the highprod2 or lowprod2 is greater than higher
+      //if true then set highprod3 to it
+  //return high3
+}
 
