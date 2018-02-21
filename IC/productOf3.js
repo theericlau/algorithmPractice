17c060fd14ceffbd7d787485766f6321e0d8d437
Eric <theericlau@gmail.com>
2018-02-20 19:11:29 -0800

    Provided Prompt

---
+++ IC/productOf3.js
@@ -0,0 +1,18 @@
+/*
+Given an array of integers,
+
+fnd the highest
+product you can get from three of the integers.
+The input arrayOfInts will always have at least three integers.
+
+Gotchas
+Does your function work with negative numbers? If arrayOfInts is [−10, −10, 1, 3, 2] we should
+return 300 (which we get by taking −10 ∗ −10 ∗ 3).
+We can do this in O(n) time and O(1) space.
+
+
+
+*/
+
+
+
