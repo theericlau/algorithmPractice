d24906e4dec96af626f44f2101c1b8e5f0050c74
Eric <theericlau@gmail.com>
2018-02-20 20:30:30 -0800

    coded out the pseudocode

--- IC/productOf3.js
+++ IC/productOf3.js
@@ -18,13 +18,34 @@ const arr = [-10, -10, 1, 3, 2];
 
 const productOf3 = (arr) => {
   //keep track of the smallest
+  let smallest = Math.min(arr[0], arr[1]);
   //keep track of the highest
+  let highest = Math.max(arr[0], arr[1]);
   //keep track of the product of first 3
+  let product3 = arr[0] * arr[1] * arr[2];
   //keep track of the min product of 2
+  let minProd2 = arr[0] * arr[1];
   //keep track of the high product of 2
+  let maxProd2 = arr[0] * arr[1];
   //loop into the array and
+  for ( let i = 2; i < arr.length; i++) {
     //check if current times the highprod2 or lowprod2 is greater than higher
+    let current = arr[i];
+
+    minProd2 = Math.min(minProd2, current * smallest);
+    maxProd2 = Math.max(maxProd2, current * highest);
+
+    if (product3 < minProd2 * current) {
+      product3 = minProd2 * current;
+    }
+    if (product3 < maxProd2 * current) {
+      product = maxProd2 * current;
+    }
       //if true then set highprod3 to it
+    smallest = Math.min(current, smallest);
+    highest = Math.max(current, highest);
+  }
   //return high3
+  return product3;
 }
 
