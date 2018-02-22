30ca8c4a3847e74dea57584c168b8e31645cd38f
Eric <theericlau@gmail.com>
2018-02-22 12:12:09 -0800

    init sumOfThree

---
+++ algo-challenges/sumOfThree.js
@@ -0,0 +1,14 @@
+/*
+Sum of Three Values
+Given an array of integers and a value, determine if there are any three integers in the array that sum equal to the given value.
+
+Consider this array and target sums.
+[ 3, 7, 1, 2, 8, 4, 5]
+
+*/
+const arr = [3, 7, 1, 2, 8, 4, 5];
+const sumOfThree = (arr) => {
+
+}
+
+console.log(sumOfThree(arr));
