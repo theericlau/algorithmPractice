c9d5a644ebbcdfb4df93b317e396bbbed13ef4e7
Eric <theericlau@gmail.com>
2018-02-22 23:49:25 -0800

    pseudocode attempt

---
+++ leetcode/62. uniquePaths.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ *
+ *
+ *
+ A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+ */
+var uniquePaths = function (m, n) {
+  var map = [],
+    temp,
+    i,
+    j;
+  //init paths for horizontal
+  //init paths for vertical
+  }
+};
