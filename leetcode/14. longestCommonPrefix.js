f8bb78ba84d120abaf56078925cad2ed9d5a48f8
Eric <theericlau@gmail.com>
2018-02-24 00:11:38 -0800

    init longestCommonPrefix

---
+++ leetcode/14. longestCommonPrefix.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+ //Write a function to find the longest common prefix string amongst an array of strings.
+
+var longestCommonPrefix = function (strs) {
+
+};
