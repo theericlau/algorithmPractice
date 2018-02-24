acb4efba3ef4ed02020fe2956e9e3a1024698165
Eric <theericlau@gmail.com>
2018-02-23 23:59:18 -0800

    going into loop

--- leetcode/13. romanToNumeral.js
+++ leetcode/13. romanToNumeral.js
@@ -4,6 +4,25 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function (s) {
 
+var romanToInt = function (s) {
+  let numeral = s.split('');
+  let total = 0;
+  let roman = {
+    'i': 1,
+    'v': 5,
+    'x': 10,
+    'l': 50,
+    'c': 100,
+    'd': 500,
+    'm': 1000,
+  }
+  for (let i = 0; i >= 0; i--) {
+    let current = numeral[i];
+    let next = numeral[i+1];
+    if (roman[next] < roman[current]) {
+      total -= roman[next];
+    }
+
+  }
 };
