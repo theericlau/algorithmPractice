2c9c5cc560ef4e7c910d94e3ab58124f063326e5
Eric <theericlau@gmail.com>
2018-02-23 23:44:02 -0800

    completed potentially

--- leetcode/9. palindromeNumber.js
+++ leetcode/9. palindromeNumber.js
@@ -1,7 +1,22 @@
 /**
  * @param {number} x
  * @return {boolean}
+ *
+ * Determine whether an integer is a palindrome. Do this without extra space.
  */
 var isPalindrome = function (x) {
-
+  let number = x.toString().split('');
+  let begin = 0;
+  let end = number.length - 1;
+  console.log(number);
+  while (end > begin) {
+    if (number[begin] !== number[end]) {
+      return false;
+    }
+    begin++;
+    end--;
+  }
+  return true;
 };
+
+console.log(isPalindrome(20));
