c051799e7c06cbc81fff803b7a97382dcc97c447
Eric <theericlau@gmail.com>
2018-02-26 19:09:37 -0800

    init function name

--- leetcode/21. mergeTwoList.js
+++ leetcode/21. mergeTwoList.js
@@ -9,3 +9,19 @@ Output: 1->1->2->3->4->4
 
 
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+
+};
