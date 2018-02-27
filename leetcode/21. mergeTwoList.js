fece2082436f10cac146e0b09f8993ef682e4fa4
Eric <theericlau@gmail.com>
2018-02-26 19:28:46 -0800

    newListnext is resetting need to fix

--- leetcode/21. mergeTwoList.js
+++ leetcode/21. mergeTwoList.js
@@ -23,5 +23,40 @@ Output: 1->1->2->3->4->4
  * @return {ListNode}
  */
 var mergeTwoLists = function (l1, l2) {
-
+  //init leftpointer
+  let leftPoint = l1;
+  //init rightpointer
+  let rightPoint = l2;
+  //init new List
+  let newList;
+  //loop until l1 and l2 are null
+  while (leftPoint || rightPoint){
+    //if newList is undefinied
+      //set newList to equal whichever is smaller l1 or l2
+    if (!newList) {
+      if (leftPoint.val <= rightPoint.val) {
+        newList = new ListNode(leftPoint.val);
+        leftPoint = leftPoint.next;
+      } else {
+        newList = new ListNode(rightPoint.val);
+        rightPoint = rightPoint.next;
+      }
+    } else {
+      //if leftpointer is less than rightpointer
+      //set next of newlist to left pointer
+      //increment leftpointer
+      if (leftPoint.val <= rightPoint.val) {
+        newList.next = leftPoint;
+        leftPoint = leftPoint.next;
+      } else {
+        //if rightpointer is less than left
+          //set next of newlist to right pointer
+          //increment rightpointer
+        newList.next = rightPoint;
+        rightPoint = rightPoint.next;
+      }
+    }
+  }
+  //return newlist
+  return newList;
 };
