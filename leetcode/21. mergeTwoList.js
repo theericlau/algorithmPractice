47fe703ea8859f632e071b308d81c11a48251157
Eric <theericlau@gmail.com>
2018-02-28 19:14:00 -0800

    Recursive Solution

--- leetcode/21. mergeTwoList.js
+++ leetcode/21. mergeTwoList.js
@@ -22,6 +22,8 @@ Output: 1->1->2->3->4->4
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+ //iterative
 var mergeTwoLists = function (l1, l2) {
   //init leftpointer
   let leftPoint = l1;
@@ -65,3 +67,20 @@ var mergeTwoLists = function (l1, l2) {
   //return newlist
   return head;
 };
+
+//recursive
+
+var mergeTwoListsRecurse = (l1, l2) => {
+  if (!l1) {
+    return l2;
+  }
+  if (!l2) {
+    return l1;
+  }
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoListsRecurse(l1.next, l2);
+  }
+  if (l1.val > l2.val) {
+    l2.next = mergeTwoListsRecurse(l1)
+  }
+}
