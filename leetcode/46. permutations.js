be9aebd8e5ba5166596f8ff39f6ef9c2b7199daa
Eric <theericlau@gmail.com>
2018-03-02 19:09:32 -0800

    Completed with exponential

--- leetcode/46. permutations.js
+++ leetcode/46. permutations.js
@@ -19,22 +19,34 @@ For example,
  */
 var permute = function (nums) {
     let init = [[]];
-    var getPerm = (arr, nums, n) {
+    var getPerm = (arr, nums, n) => {
       //init result;
+      let result = [];
         //base case
+      if (n === nums.length) {
+        return arr;
+      }
         //if n = nums.length
         //stop recurse
-
+      let current = nums[n];
         //let current = nums[n];
 
         //loop into outer nums
+      for (let i = 0; i < arr.length ; i++ ){
+        for (let j = 0; j <= arr[0].length; j++) {
+          let copy = arr[i].slice();
+          copy.splice(j, 0, current);
+          result.push(copy);
+        }
+      }
             //loop into inner nums
             //create a copy
             //insert current around the digits
             //push to result
         //return getPerm with result and next index
-
-
+      return getPerm(result, nums, n+1);
     }
     return getPerm(init, nums, 0);
 };
+
+console.log(permute([1,2]));
