16115b57726e03c4012b030069d6a329d14d3c52
Eric <theericlau@gmail.com>
2018-03-02 18:47:43 -0800

    BackTrack with permute pseudocode

--- leetcode/46. permutations.js
+++ leetcode/46. permutations.js
@@ -18,5 +18,23 @@ For example,
  * @return {number[][]}
  */
 var permute = function (nums) {
+    let init = [[]];
+    var getPerm = (arr, nums, n) {
+      //init result;
+        //base case
+        //if n = nums.length
+        //stop recurse
 
+        //let current = nums[n];
+
+        //loop into outer nums
+            //loop into inner nums
+            //create a copy
+            //insert current around the digits
+            //push to result
+        //return getPerm with result and next index
+
+
+    }
+    return getPerm(init, nums, 0);
 };
