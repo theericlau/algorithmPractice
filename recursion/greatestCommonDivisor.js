a42ad19a865471d7a79f4107270cc11b06bf2a69
Eric <theericlau@gmail.com>
2018-03-05 13:57:47 -0800

    working on the naive approach

--- recursion/greatestCommonDivisor.js
+++ recursion/greatestCommonDivisor.js
@@ -1,3 +1,24 @@
 /*
 Write a function that takes two numbers and returns the greatest common divisor.
 */
+
+const greatestCommonDivisor = (num1, num2) => {
+    //storage of what gets divisible by num1; Only need to check to sqrt of bigger number;
+    //init GCD
+    let gcd;
+    //loop into Math.sqrt of bigger number
+    for (let i = 0; i < Math.sqrt(Math.max(num1, num2)); i ++) {
+        if (num1)
+    }
+        //check who
+    //return GCD
+    return gcd;
+}
+
+
+36
+1: 36
+2: 18
+3: 12
+4: 9
+6: 6
