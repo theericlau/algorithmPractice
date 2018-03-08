f0a55f041ef73d5917031d2413469f2ba057f951
Eric <theericlau@gmail.com>
2018-03-08 00:00:40 -0800

    Memoized version of cakeTheif

--- IC/cakeThief.js
+++ IC/cakeThief.js
@@ -50,11 +50,38 @@ const maxDuffelBagValue = (cakeTypes, capacity) => {
                 checkForMax(capacityLeft - cakeTypes[i].weight, value + cakeTypes[i].value);
             }
         }
-
     }
     //return the max
     checkForMax(capacity, 0);
     return max;
 }
 
-console.log(maxDuffelBagValue(cakeTypes, 20));
+
+const bag = (types, capacity) => {
+    //init an array with maxValue at every capacity
+    let maxValuesAtCapacity = [];
+    //init the array
+    for (let i = 0; i <= capacity; i++) {
+        maxValuesAtCapacity[i] = 0;
+    }
+    for (let currentCapacity = 0 ; currentCapacity <= capacity; currentCapacity++ ) {
+        let currentMax = 0;
+        for (let j = 0 ; j< types.length; j++) {
+            let type = types[j];
+            if (type.weight === 0 && type.value > 0) {
+                return Infinity;
+            }
+            if (type.weight <= currentCapacity) {
+                let maxValueUsingType = type.value + maxValuesAtCapacity[currentCapacity- type.weight];
+                currentMax = Math.max(maxValueUsingType, currentMax);
+            }
+        }
+        maxValuesAtCapacity[currentCapacity] = currentMax;
+    }
+    console.log(maxValuesAtCapacity);
+    return maxValuesAtCapacity[capacity];
+}
+
+// console.log(maxDuffelBagValue(cakeTypes, 20));
+
+console.log(bag(cakeTypes, 6));
