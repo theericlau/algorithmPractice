17a11873d0087ca188e24c0040d6dad27f0e329b
Eric <theericlau@gmail.com>
2018-03-07 19:12:02 -0800

    recurse solution

--- algo-challenges/getStringFromMessage.js
+++ algo-challenges/getStringFromMessage.js
@@ -7,7 +7,26 @@ var message = `Feel free to send a pull request to add more strings, or addition
 Likewise, please do not send pull requests which compromise manual usability of the file.This includes the EICAR test string, which can cause the file to be flagged by antivirus scanners, and files which alter the encoding of blns.txt.Also, do not send a null character(U + 0000) string, as it changes the file format on GitHub to binary and renders it unreadable in pull requests.Finally, when adding or removing a string please update all files when you perform a pull request.`
 
 var messageParse = (message, length) => {
-
+  let array = [];
+  let string = '';
+  let recurse = (message) => {
+    if (string.length === length) {
+      console.log('string');
+      array.push(string);
+      string = '';
+    }
+    // for (let i = 0; i < message.length; i++) {
+    string = string.concat(message[0]);
+    message = message.slice(1);
+    if (message) {
+      recurse(message);
+    }
+  }
+  recurse(message);
+  return array;
 }
 
-console.log(messageParse(message, 20));
+
+
+
+console.log(messageParse(message, 150));
