2ce42c1e317bb8aa0aaea6f33f44abd0d3270e42
Eric <theericlau@gmail.com>
2018-03-11 18:16:57 -0700

    Using sort to minimize space complexity

--- IC/spaceEdition.js
+++ IC/spaceEdition.js
@@ -11,11 +11,50 @@ We're going to run this function on our new, super-hip Macbook Pro With Retina D
 */
 
 //Naive Solution
-const test = [1,2,3,4,5,6,7,1,2]
 
 const duplicate = array => {
   //initialize the object which will count whether its in the object already
+  const track = {};
+  const repeat = [];
   //loop in to the array
+  for (let i = 0; i < array.length; i++) {
+    let current = array[i];
     //keep track of how many times the in
+    if (track[current]) {
+      repeat.push(current);
+    }
+    else {
+      track[current] = true;
+    }
     //if number was already seen move into an array repeated object
+
+  }
+  return repeat;
 }
+
+//Simplify
+
+const test = [1,2,3,4,5,6,7,1,2]
+const duplicateSimple = array => {
+  const track = {};
+  return array.filter( curr => {
+    if (track[curr]){
+      return true
+    } else {
+      track[curr] = true;
+      return false
+    }
+  });
+}
+
+const duplicateSpace = array => {
+  array
+  .sort((a, b) => {
+    return a - b;
+  })
+  let next = 0;
+  return array;
+}
+
+// console.log(duplicateSimple(test));
+console.log(duplicateSpace(test));
