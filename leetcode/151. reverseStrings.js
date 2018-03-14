4cf5a53b09fe912956450c338d41d4cd6d86780d
Eric <theericlau@gmail.com>
2018-03-13 20:48:06 -0700

    pseudocoded the function;

--- leetcode/151. reverseStrings.js
+++ leetcode/151. reverseStrings.js
@@ -12,6 +12,15 @@ return "blue is sky the".
  * @param {string} str
  * @returns {string}
  */
-var reverseWords = function (str) {
 
+var swap = (array, index1, index2) => {
+  [array[index1], array[index2]] = [array[index2], array[index1]]
+}
+var reverseWords = function (str) {
+  //split the string by the spaces
+  //keep track of begin and end;
+  //while the begin and end are not equal
+    //swap begin and end
+
+  //join the array;
 };
