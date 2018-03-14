b99387f2737992894b971fc4d580afac3bc63fe8
Eric <theericlau@gmail.com>
2018-03-13 20:23:00 -0700

    pseudocoded

--- leetcode/75. sortColor.js
+++ leetcode/75. sortColor.js
@@ -12,6 +12,23 @@ You are not suppose to use the library's sort function for this problem.
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function (nums) {
 
+var swap = (array, index1, index2) => (
+  [array[index1], array[index2]] = [array[index2], array[index1]];
+);
+
+var sortColors = function (nums) {
+  //have pointer of begin
+  //have pointer of middle
+  //have pointer of end
+  //loop into the nums
+    //if current is 0
+      //swap to front
+      //increase begin
+      //increase middle
+    //if current is 1
+      //increase middle
+    //if current is 2
+      //swap to the end
+      //swap to end
 }
