449b6381571af5bc4490b109982b84005070dba1
Eric <theericlau@gmail.com>
2018-03-15 16:13:14 -0700

    init rotatedarray

---
+++ algo-challenges/rotatedArray.js
@@ -0,0 +1,11 @@
+/*
+Given an array and another array with the rotation of it. Return an array with the index of where the max is after each rotation
+ex)
+arr = [1,2,3]
+rotateArr = [1,1,1,2]
+
+returns
+[1, 0, 2, 0]
+
+*/
+
