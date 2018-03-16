775227a0db69490a60b79c14c6078ec1d17a3a17
Eric <theericlau@gmail.com>
2018-03-15 17:05:46 -0700

    Complete removeDup

--- leetcode/26. removeDuplicateFromArray.js
+++ leetcode/26. removeDuplicateFromArray.js
@@ -16,7 +16,61 @@ It doesn't matter what you leave beyond the new length.
  * @return {number}
  */
 
-var nums = [1, 1, 2];
-var removeDuplicates = function (nums) {
+var nums = [1, 1, 2, 3, 3, 5];
+// var removeDuplicates = function (nums) {
+//   let begin = 0;
+//   let next = 1;
+//   //loop into the array
+//   //if previous is the same as current remove
+//   while (begin < nums.length){
+//     if (nums[begin] === nums[next]) {
+//       nums.splice(next, 1);
+//     } else {
+//       begin++
+//       next++
+//     }
+//   }
+//   return nums;
+// };
 
-};
+var removeDuplicates = function(nums) {
+  let length = 0;
+  if (nums.length === 0 ) {
+    return length;
+  }
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[length] === nums[i]) {
+      continue;
+    } else {
+      nums[length + 1] = nums[i];
+      length++;
+    }
+  }
+  console.log(nums);
+  return length + 1;
+}
+
+
+// var removeDuplicates = function (nums) {
+//   var len = nums.length,
+//     cur,
+//     i, // original array's index
+//     j; // new array's index
+
+//   if (len === 0) {
+//     return 0;
+//   }
+
+//   cur = nums[0];
+
+//   for (i = 1, j = 0; i < len; i++) {
+//     if (nums[i] !== cur) {
+//       j++;
+//       nums[j] = nums[i];
+//       cur = nums[i];
+//     }
+//   }
+//   console.log('im the array', nums);
+//   return j + 1;
+// };
+console.log(removeDuplicates(nums));
