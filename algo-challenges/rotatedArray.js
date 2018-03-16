079391c910ab0d12689fd7eefa8a54f058ca81d5
Eric <theericlau@gmail.com>
2018-03-15 23:50:55 -0700

    Optimized rotations

--- algo-challenges/rotatedArray.js
+++ algo-challenges/rotatedArray.js
@@ -12,20 +12,39 @@ returns
 
 
 const getIndexOfMaxAfterRotated = (arr, rotate) => {
+  debugger;
   let result = [];
   let max = Math.max(...arr);
   for (let i = 0; i< rotate.length; i++) {
-    rotateHelper(arr, rotate[i]);
+    rotateHelperTimes(arr, rotate[i]);
     result.push(arr.indexOf(max));
   }
   return result;
 }
 
-const rotateHelper = (arr, numOfTime) => {
+const rotateHelperTimes = (arr, numOfTime) => {
   for (let i = 0; i < numOfTime; i++) {
-    arr.push(arr[0]);
-    arr.shift();
+    // arr.push(arr[0]);
+    // arr.shift();
+    rotateHelper(arr);
   }
 }
 
+const rotateHelper  = arr => {
+  let curr = 0;
+  let next = 1;
+  if (arr.length > 1) {
+    while (next < arr.length) {
+      [arr[curr], arr[next]] = [arr[next], arr[curr]];
+      curr++;
+      next++;
+    }
+    return arr;
+  } else {
+    return arr;
+  }
+}
+// console.log(rotateHelper([1,2,3]));
+
 console.log(getIndexOfMaxAfterRotated([1,2,3], [1,1,1,2]));
+
