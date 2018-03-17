e1c15fb44fe4f7ef01106946b90f2f2b5f99992d
Eric <theericlau@gmail.com>
2018-03-16 22:16:02 -0700

    init calculator challenge

---
+++ algo-challenges/calculatorExtra.js
@@ -0,0 +1,10 @@
+/*
+
+I forget exactly what they called the calculator, but the question was essentially to code a RPN calculator that incorporates parenthesis and an indeterminate amount of numbers per operation (where as RPN will always be only two numbers per operation). An example being "( add 1 ( mul 3 4 ) )" = 13.
+
+
+*/
+
+const calculator = string => {
+
+}
