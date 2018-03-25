1031bc098e1cdb42b545c8f1948a31a39e4c7659
Eric <theericlau@gmail.com>
2018-03-24 19:36:05 -0700

    Cleaned up code and added comments

--- algo-challenges/graphTest/index.js
+++ algo-challenges/graphTest/index.js
@@ -1,43 +1,3 @@
-/*
-Introduction:
-There is a group of people (lets say 30)
-Each person has multiple names by which they can be identified. Lets say each person has atleast 1 name and 3 names max. Some may have only 1 name and some may have 3.
-Each person is related to others in the group either DIRECTLY or INDIRECTLY. DIRECTLY means that persons knows the other person name. INDIRECTLY means the person knows a person who knows the other person. This could be
-Single-hop, eg, A knows B know C
-
-OR
-
-Multi-hop eg: A knows B knows D knows E knows C. And it means A knows C via 3 hops
-
-Since every person has multiple names, it could be that A knows C via
-A3 -> B1 -> E3 -> D2 -> C2
-
-                Where the number denotes one of the 3 names.
-
-Only DIRECT relationships are specified on input
-There could be people who are not related to anybody
-There should be no loops in the relationship, ie A3 -> B1 -> E3 -> D2 -> E3 ->C2 where E3 shows up twice.
-
-
-Problem statement
-Display all the relationships EACH person has to others either DIRECTLY or INDIRECTLY using each of the 3 possible names.
-The display can be simple text like shown in #4 above OR in some UI framework if that is easier
-
-You are free to choose the programing language to implement this.
-
-Please generate the input data for 30 people with max 5 hop relationships. This can be represented in any form (simple text, JSON etc). Where each person is defined as
-A1 <-> C2
-A1 <-> D3
-A2 <-> B1
-
-B2 <-> D1
-D1 <-> C3
-D2 <-> E1
-
-F2 (eg of a person not related to anyone)
-etc
-*/
-
 class Graph {
   constructor() {
     this.storage = {};
@@ -60,7 +20,6 @@ class Graph {
         }
       }
     }
-
   }
 
   // Removes a node from the graph.
@@ -78,9 +37,9 @@ class Graph {
     }
   }
 
+  // Counts the number of nodes.
   numNode(node) {
-    let length = Object.keys(this.storage[node]).length;
-    return length;
+    return Object.keys(this.storage[node]).length;
   }
 
 
@@ -119,6 +78,7 @@ class Graph {
     delete this.storage[toNode[0]][toNode].edges[fromNode];
   }
 
+  // Print all direct relationships.
   printDirects() {
     const storage = this.storage;
     let all = [];
@@ -137,11 +97,13 @@ class Graph {
     return all;
   }
 
+  // Print all indirect relationships.
   printIndirects() {
     const storage = this.storage;
     let all = [];
     let length = Object.keys(storage).length;
 
+    /// Inner recursion to create all permutations for indirect relationships
     const generateIndirect = (total, node, currentArr) => {
       currentArr.push(node);
       let keys = Object.keys(storage[node[0]][node].edges);
@@ -155,7 +117,6 @@ class Graph {
         if (currentArr.indexOf(keys[i]) >= 0) {
           continue;
         } else {
-          // Recurse through each permutation
           generateIndirect(total, keys[i], currentArr.concat());
         }
       }
@@ -168,6 +129,7 @@ class Graph {
     return all;
   }
 
+  // Generate random nodes with multiple names for the graph.
   generateNode() {
     const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     for (let i = 0; i < alpha.length; i++) {
@@ -179,6 +141,7 @@ class Graph {
     }
   }
 
+  // Generate edges for the nodes and makes sure there's no edges between same node
   generateEdges(){
     let keys = Object.keys(this.storage);
     //Randomly select toNode and fromNode
@@ -193,37 +156,8 @@ class Graph {
 
 }
 
+// Helper function for generateNode
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-
-// const connects = new Graph();
-// connects.addNode('A');
-// connects.addNode('A');
-// connects.addNode('B');
-// connects.addNode('C');
-// connects.addNode('C');
-// connects.addNode('D');
-// connects.addNode('E');
-// connects.addNode('F');
-
-
-// console.log(connects.contains('A1'));
-// connects.deleteNode('A2');
-// console.log(connects.contains('A2'));
-// console.log(connects.storage);
-// connects.generateNode();
-// connects.generateEdges();
-// console.log(connects.storage);
-// connects.addEdge('A1', 'B1');
-// connects.addEdge('A1', 'D1');
-// connects.addEdge('B1', 'C1');
-// connects.addEdge('C1', 'D1');
-// connects.addEdge('C1', 'E1');
-// connects.addEdge('C2', 'E1');
-// connects.addEdge('E1', 'F1');
-// connects.addEdge('F1', 'A1');
-// connects.addEdge('B1', 'E1');
-// console.log('Direct', connects.printDirects());
-// console.log('Indirect', connects.printIndirects());
 
