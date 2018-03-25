5ba852b9e977cabb68d4903b0cea1451b428d59c
Eric <theericlau@gmail.com>
2018-03-24 19:11:35 -0700

    deleted and modified store

--- algo-challenges/graphTest/app.js
+++
@@ -1,8 +0,0 @@
-import Graph from "./index";
-const createNode = name => {
-  console.log(name);
-}
-
-const connect = new Graph();
-console.log(connect);
-document.getElementById('buttonCreate').addEventListener("click", createNode);
