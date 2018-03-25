f3cf85d096420f215b6d0fdd7c53cb450843e602
Eric <theericlau@gmail.com>
2018-03-25 15:41:28 -0700

    pseudocoded problem

--- leetcode/300. longestIncreasingSubsequence.js
+++ leetcode/300. longestIncreasingSubsequence.js
@@ -16,6 +16,21 @@ Follow up: Could you improve it to O(n log n) time complexity?
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function (nums) {
 
+var lengthOfLIS = function (nums) {
+  //init length
+  //loop into array to keep track of beginning of subsequence
+    //init count to 1
+    //init current to begin of array
+    //loop into array to keep check if next is larger
+      //if larger
+        //set current to this #
+        //increment count
+    //if count > length
+      //set length to count
+  //return length
 };
+
+
+
+let test = [10, 9, 2, 5, 3, 7, 101, 18];
