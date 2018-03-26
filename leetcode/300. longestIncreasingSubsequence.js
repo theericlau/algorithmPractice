806898ff1eb508721b1e61e637059ace96f464ad
Eric <theericlau@gmail.com>
2018-03-25 17:40:44 -0700

    completed LIS

--- leetcode/300. longestIncreasingSubsequence.js
+++ leetcode/300. longestIncreasingSubsequence.js
@@ -53,12 +53,30 @@ var lengthOfLIS = function (nums) {
 var lengthOfLIS2 = function(nums) {
   //init memoized way
   //init max
+  let memoize = [];
+  let max = 0;
   //loop into array to keep track of end
+    //initialize with 1
     //loop into array to check if smaller than end
       //if smaller than end then max
-        //check if it is longest is greater than what it is currently at;
+        //check if it is longest is greater than what it is currently at; and also check even if current smaller than end to only set to equal if the memoize is greater
     //if this length is bigger than current max then replace
+  for (let i = 0; i < nums.length; i++) {
+    memoize[i] = 1;
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i] && memoize[j] + 1 > memoize[i]) {
+        // if (memoize[]) {
+          memoize[i] = memoize[j] + 1;
+        // }
+      }
+    }
+    if (memoize[i] > max) {
+      max = memoize[i];
+    }
+  }
+
   //return max
+  return max
 }
 
 
@@ -66,5 +84,5 @@ var lengthOfLIS2 = function(nums) {
 
 let test = [10, 9, 2, 5, 3, 7, 101, 18];
 let test2 = [10, 20, 50, 16, 11, 12, 13, 14];
-
-console.log(lengthOfLIS2(test2));
+let test3 = [1, 3, 6, 7, 9, 4, 10, 5, 6]
+console.log(lengthOfLIS2(test3));
