7b0942d4da57d370418a62622b1a87a07cdb71e1
Eric <theericlau@gmail.com>
2018-03-30 20:43:10 -0700

    minor bug with 3 nodes

--- leetcode/24. swapNodesInPairs.js
+++ leetcode/24. swapNodesInPairs.js
@@ -19,9 +19,53 @@ Your algorithm should use only constant space. You may not modify the values in 
  * @return {ListNode}
  */
 var swapPairs = function (head) {
+  debugger;
   //make a loop that
     //swap next with current
     //make current into current.next.next
+  if (!head || !head.next) {
+    return head;
+  }
+  let next = head.next;
+  let current = head;
+  while (current && current.next) {
+    let nextNode = current.next.next;
+    swap(current, current.next);
+    current = nextNode;
+  }
 
   //return head's next
+  return next
 };
+
+let swap = (node1, node2) => {
+  if (node2.next){
+    node1.next = node2.next.next;
+  } else {
+    node1.next = node2.next;
+  }
+  node2.next = node1;
+
+}
+
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var A = new ListNode('A')
+var B = new ListNode('B')
+var C = new ListNode('C')
+var D = new ListNode('D')
+
+A.next = B;
+B.next = C;
+C.next = D;
+
+console.log(swapPairs(A))
+
+
+
+
+
