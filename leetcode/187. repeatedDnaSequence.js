fcd86d2025cf55268fee313f5045d54823d16498
Eric <theericlau@gmail.com>
2018-03-30 18:46:12 -0700

    accounted for the 11 digits

--- leetcode/187. repeatedDnaSequence.js
+++ leetcode/187. repeatedDnaSequence.js
@@ -17,9 +17,33 @@ For example,
  * @return {string[]}
  */
 var findRepeatedDnaSequences = function (s) {
+  let list = [];
+  let hash = {};
   //loop into strings
     //store every substring in hash
     //if exists then increment in hash
+  for (let i = 0; i <= s.length - 10; i++) {
+    let substring = s.substring(i, i + 10);
+    if (!hash[substring]) {
+      hash[substring] = 1
+    } else {
+      if (hash[substring] === 1) {
+        list.push(substring);
+        hash[substring]++
+      }
+    }
+  }
   //loop in hash to check which dna has multiple
+  // for (let substring in hash) {
+  //   if (hash[substring] > 1) {
+  //     list.push(substring);
+  //   }
+  // }
   //return list
+  return list;
 };
+
+let test = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+let test2 = "AAAAAAAAAAA";
+console.log(findRepeatedDnaSequences(test));
+console.log(findRepeatedDnaSequences(test2));
