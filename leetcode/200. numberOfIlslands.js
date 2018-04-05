6396612240d0d693f0206c7fa0d7a15d1976917a
Eric <theericlau@gmail.com>
2018-04-04 20:10:23 -0700

    Completed numberofIslands

--- leetcode/200. numberOfIlslands.js
+++ leetcode/200. numberOfIlslands.js
@@ -27,6 +27,17 @@ Answer: 3
  */
 var numIslands = function (grid) {
   //init total
+  debugger;
+  let total = 0;
+  for ( let i = 0; i < grid.length; i ++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === "1") {
+        total++;
+        dfs(grid, i, j);
+      }
+    }
+  }
+  return total
   //loop into each array
     //check if adjacent are also 1s
       //if current is 1 increment total
@@ -34,3 +45,16 @@ var numIslands = function (grid) {
   //return total
 };
 
+const dfs = (grid, i, j) => {
+  if (i < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] === "0") {
+    return;
+  }
+  grid[i][j] = "0";
+  dfs(grid, i-1, j);
+  dfs(grid, i+1, j);
+  dfs(grid, i, j+1);
+  dfs(grid, i, j-1);
+}
+
+let grid = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]
+console.log(numIslands(grid));
