98d02feafa7fb404eaa60febc1111224380d5828
Eric <theericlau@gmail.com>
2018-04-04 19:53:02 -0700

    completed Search

--- leetcode/33. searchInRotatedSortedArray.js
+++ leetcode/33. searchInRotatedSortedArray.js
@@ -16,16 +16,57 @@ You may assume no duplicate exists in the array.
  */
 var search = function (nums, target) {
   //init the begin, end and mid index
+  // debugger;
+  let begin = 0;
+  let end = nums.length - 1;
+  let mid;
   //while mid is greater than end
+  while (end - begin > 1) {
     //every time halve it
+    mid = Math.floor((end + begin) / 2 );
+    let current = nums[mid];
+    let beginNum = nums[begin];
+    let endNum = nums[end];
     //if (current mid= target) then return mid
+    if (current === target) {
+      return mid;
+    }
     //if (begin < mid && target is greater than begin)
       // if target greater than mid) then set begin to mid
       // else set end to mid
+    if (beginNum < current) {  //&& target > beginNum
+      if (target > current || target < beginNum){
+        begin = mid;
+      } else {
+        end = mid;
+      }
+    } else {
+      if (target < current || target > endNum) {
+        end = mid;
+      } else {
+        begin = mid;
+      }
+    }
+  }
     //if (end > mid && target is greater than end)
     //else
       // target greater than end then end = mid
       //else
         //then set begin to mid
+
+    //case of 2 number
+  if (nums[begin] === target) {
+    return begin;
+  } else if (nums[end] === target) {
+    return end;
+  } else {
+    return -1;
+  }
+}
   //return -1
-};
+  // return -1;
+
+let test = [10,2,3,5, 6, 8, 9];
+let test1 = [3,5,1];
+// console.log(search(test, 8));
+console.log(search(test1, 1));
