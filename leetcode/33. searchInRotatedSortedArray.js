88907a64c5e78a1c4954035cebcccd5ae3426941
Eric <theericlau@gmail.com>
2018-04-04 18:48:17 -0700

    pseudocoded out solution

--- leetcode/33. searchInRotatedSortedArray.js
+++ leetcode/33. searchInRotatedSortedArray.js
@@ -15,5 +15,17 @@ You may assume no duplicate exists in the array.
  * @return {number}
  */
 var search = function (nums, target) {
-
+  //init the begin, end and mid index
+  //while mid is greater than end
+    //every time halve it
+    //if (current mid= target) then return mid
+    //if (begin < mid && target is greater than begin)
+      // if target greater than mid) then set begin to mid
+      // else set end to mid
+    //if (end > mid && target is greater than end)
+    //else
+      // target greater than end then end = mid
+      //else
+        //then set begin to mid
+  //return -1
 };
