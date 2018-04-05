4957983a9398824fb859a9a8c800a8220b3d424b
Eric <theericlau@gmail.com>
2018-04-04 17:39:17 -0700

    testing out mapfilter

--- algo-challenges/test.js
+++ algo-challenges/test.js
@@ -16,3 +16,47 @@ function findNumber(arr, k) {
     return 'NO'
   }
 }
+
+
+var factories = [
+
+  {
+    'factory name': 'c&h plush',
+    'products': 'stuffed animals',
+    'rating': 'B',
+    'VIP': false,
+    'monthly capacity': 100000,
+    'total current orders': 10000,
+    'days to deliver': 20,
+    'shipping port': ['yantian']
+  },
+  {
+    'factory name': 'Fox Con',
+    'products': 'electronics',
+    'rating': 'A',
+    'VIP': true,
+    'monthly capacity': 1000000,
+    'total current orders': 1000000,
+    'days to deliver': 10,
+    'shipping port': ['yantian', 'shanghai', 'beijing']
+  },
+  {
+    'factory name': 'SS clothing',
+    'products': 'textiles',
+    'rating': 'F',
+    'VIP': true,
+    'monthly capacity': 10000,
+    'total current orders': 0,
+    'days to deliver': 30,
+    'shipping port': ['yantian']
+  }
+
+];
+
+const getFactoryWithVIP = (array) => {
+  return array.filter(each=> {
+    return each.VIP && each['monthly capacity'] > 1000
+  }).map((data) => data['factory name'])
+}
+
+console.log(getFactoryWithVIP(factories));
