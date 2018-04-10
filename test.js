1844781b7a9ae081523b0d78ec3a479b61fceb83
Eric <theericlau@gmail.com>
2018-04-09 20:30:51 -0700

    created default test cases

---
+++ test.js
@@ -0,0 +1,24 @@
+// const test = (a,b) => {
+// // debugger;
+//   return a+b;
+
+// }
+// test(1,2);
+
+
+function foo1()
+{
+  return {
+    bar: "hello"
+  };
+}
+
+function foo2()
+{
+  return(
+    {
+      bar: "hello"
+    }
+  )
+}
+
