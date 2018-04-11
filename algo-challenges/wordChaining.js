4275415bacdde6cc75b8bc919035ec1428bbeb03
Eric <theericlau@gmail.com>
2018-04-11 00:24:10 -0700

    init wordchaining

---
+++ algo-challenges/wordChaining.js
@@ -0,0 +1,9 @@
+/*
+Figure out whether the given words can form a circular chain. Assume that single word can never form a chain.
+
+Two words can be chained together if the first word's last letter is equal to the second word's first letter. We are given a list of words and we need to figure out if all these words can be chained together or not.  Let's assume that all words are lower case.
+
+[eve, eat, ripe, tear];
+
+*/
+
