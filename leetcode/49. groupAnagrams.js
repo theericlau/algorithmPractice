0d57795a8bd7fb50eeeedf14a068498c1f39d341
Eric <theericlau@gmail.com>
2018-04-11 16:18:08 -0700

    init groupanagrams

---
+++ leetcode/49. groupAnagrams.js
@@ -0,0 +1,20 @@
+/*
+
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+*/
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+
+};
