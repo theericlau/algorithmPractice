1334dcefe4035d8388a08e4d994e916610350edb
Eric <theericlau@gmail.com>
2018-04-11 19:59:35 -0700

    completed

--- leetcode/49. groupAnagrams.js
+++ leetcode/49. groupAnagrams.js
@@ -5,6 +5,8 @@ Given an array of strings, group anagrams together.
 For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
 Return:
 
+aet  aet
+
 [
   ["ate", "eat","tea"],
   ["nat","tan"],
@@ -29,5 +31,30 @@ var groupAnagrams = function (strs) {
     array.push(obj);
   }
 
+  for (let i = 0; i< array.length; i++) {
+    //for each object check if
+  }
+
 
 };
+
+const groupAnagrams2 = strs => {
+  let sorted = [];
+  let result = {};
+  for (let i = 0; i < strs.length; i++) {
+    const wordSort = strs[i].split('').sort().join('');
+    if (result.hasOwnProperty(wordSort)) {
+      result[wordSort].push(strs[i]);
+    } else {
+      result[wordSort] = [strs[i]];
+    }
+  }
+  for (let key in result) {
+    sorted.push(result[key]);
+  }
+  return sorted;
+}
+
+array = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+console.log(groupAnagrams2(array))
