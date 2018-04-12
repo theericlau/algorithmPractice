0c45b3aa79e1df4648ab015ab85db0e33a4c9425
Eric <theericlau@gmail.com>
2018-04-11 18:34:34 -0700

    started the problem but bad time o(n)

--- leetcode/49. groupAnagrams.js
+++ leetcode/49. groupAnagrams.js
@@ -16,5 +16,18 @@ Return:
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
+  let array = [];
+  for (let i = 0; i < strs.length; i ++) {
+    let obj = {};
+    for (let j = 0; j < strs[i].length; j++) {
+      if (obj.hasOwnProperty(str[i][j])){
+        obj[str[i][j]] += 1;
+      } else {
+        obj[str[i][j]] = 1;
+      }
+    }
+    array.push(obj);
+  }
+
 
 };
