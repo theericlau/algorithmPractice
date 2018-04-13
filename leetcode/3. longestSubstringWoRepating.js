a1649b68af262b53ce2de0d612dd9704519c8665
Eric <theericlau@gmail.com>
2018-04-13 16:51:55 -0700

    still working on splitting substrings

--- leetcode/3. longestSubstringWoRepating.js
+++ leetcode/3. longestSubstringWoRepating.js
@@ -17,5 +17,32 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
  */
 var lengthOfLongestSubstring = function (s) {
 
-
+  let max = 1;//1
+  let string = s[0]; //p
+  for (let i = 0; i < s.length;i++) {
+    let current = 1;  //1
+    let obj = {};
+    obj[s[i]] = 1; //p: 1 w: 1
+    for (let j = i + 1; j < s.length; j++ ){ //w 2
+      if (obj.hasOwnProperty(s[j])){  //
+        if (current > max){
+          max = current;
+          string = s.substring(i, i + current)  //0,3
+        }
+        break;
+      } else {
+        current ++;
+        obj[s[j]] = 1;
+      }
+    }
+  }
+  return string;
 };
+
+string1 = "abcabcbb";
+string2 = "bbbbb";
+string3 = "pwwkew";
+
+console.log(lengthOfLongestSubstring(string1))
+console.log(lengthOfLongestSubstring(string2))
+console.log(lengthOfLongestSubstring(string3))
