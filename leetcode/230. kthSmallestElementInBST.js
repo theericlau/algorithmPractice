b86b66c0b90c819d46bf9274ffd0feb03e95143f
Eric <theericlau@gmail.com>
2018-04-30 16:40:18 -0700

    added kthsmallestElementinBST

---
+++ leetcode/230. kthSmallestElementInBST.js
@@ -0,0 +1,35 @@
+/*
+
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+
+function TreeNode(val) {
+ this.val = val;
+ this.left = this.right = null;
+}
+
+var kthSmallest = function (root, k) {
+
+};
