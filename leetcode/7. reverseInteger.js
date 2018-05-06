b2028c427507cabcd38537c9fe153c17a295b01c
Eric <theericlau@gmail.com>
2018-05-06 14:05:03 -0700

    Completed reverseInteger

--- leetcode/7. reverseInteger.js
+++ leetcode/7. reverseInteger.js
@@ -15,7 +15,7 @@ Example 3:
 Input: 120
 Output: 21
 Note:
-Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows2™
 
 */
 
@@ -24,5 +24,24 @@ Assume we are dealing with an environment which could only store integers within
  * @return {number}
  */
 var reverse = function (x) {
-
+  let negative = x < 0 ? true : false;
+  let newX = Math.abs(x).toString();
+  let start = 0;
+  let end = newX.length - 1;
+  while (end > start) {
+    newX = swap(start, end, newX);
+    end--;
+    start++;
+  }
+  return negative ? Number(newX) * -1 : Number(newX);
 };
+
+var swap = (begin, end, x) => {
+  let start = x.slice(0, begin);
+  let mid = x.slice(begin+1, end);
+  let last = x.slice(end + 1);
+  return start.concat(x[end],mid,x[begin], last);
+};
+
+console.log(reverse(-123456));
+// console.log(swap(2, 4, '123456'));
